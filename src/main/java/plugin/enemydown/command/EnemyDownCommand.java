d17f4afd7cbdecd7752425cd7c06b3cd1f6f9c21
piko <piko@piko-MBA15.local>
2024-03-23 20:16:57 +0900

    ・Day22のリファクタリングを実施 ・不要な変数であるworldを削除 ・onExecutePlayerCommandからgamePlayメソッドを抽出 ・EnemyDownCommandのJavaDocを追記 ・ゲーム時間をgetPlayerScoreから取得するように修正し、合わせて三項演算子で処理の簡略化

---
+++ src/main/java/plugin/enemydown/command/EnemyDownCommand.java
@@ -0,0 +1,180 @@
+package plugin.enemydown.command;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.SplittableRandom;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import plugin.enemydown.Main;
+import plugin.enemydown.data.PlayerScore;
+
+/**
+ * 制限時間内に敵を出現させて、倒した敵に応じてスコアを集計し、表示させるコマンド
+ * 結果はプレイヤー名、点数、日時などで表示されます
+ */
+
+public class EnemyDownCommand extends BaseCommand implements CommandExecutor, Listener {
+
+  public static final int GAME_TIME = 20;
+  private Main main;
+  private List<PlayerScore> playerScoreList = new ArrayList<>();
+
+  public EnemyDownCommand(Main main) {
+    this.main = main;
+  }
+
+  @Override
+  public boolean onExecutePlayerCommand(Player player) {
+    PlayerScore nowPlayer = getPlayerScore(player);
+    initPlayerStatus(player);
+
+    gamePlay(player, nowPlayer);
+    return false;
+  }
+
+  @Override
+  public boolean onExecuteNPCCommand(CommandSender sender) {
+    return false;
+  }
+
+  /**
+   * 新規のプレイヤー情報をリストに追加します
+   *
+   * @param player コマンドを実行したプレイヤー
+   * @return  新規プレイヤー
+   */
+  private PlayerScore addNewPlayer(Player player) {
+    PlayerScore newPlayer = new PlayerScore(player.getName());
+    playerScoreList.add(newPlayer);
+    return newPlayer;
+  }
+
+  @EventHandler
+  public void onEnemyDeath(EntityDeathEvent e) {
+    LivingEntity enemy = e.getEntity();
+    Player player = enemy.getKiller();
+    if (Objects.isNull(player) || playerScoreList.isEmpty()) {
+      return;
+    }
+
+    for(PlayerScore playerScore : playerScoreList) {
+      if (playerScore.getPlayerName().equals(player.getName())) {
+        int point = switch (enemy.getType()) {
+          case ZOMBIE, SPIDER -> 10;
+          case SKELETON -> 20;
+          default -> 0;
+        };
+        playerScore.setScore(playerScore.getScore() + point);
+        player.sendMessage("敵を倒した！現在のスコアは" + playerScore.getScore() +"点");
+      }
+    }
+  }
+
+  /**
+   * 現在実行しているプレイヤーのスコア情報を取得する。
+   * @param player コマンドを実行したプレイヤー
+   * @return 現在実行しているプレイヤーのスコア情報
+   */
+  private PlayerScore getPlayerScore(Player player) {
+    PlayerScore playerScore = new PlayerScore(player.getName());
+    if (playerScoreList.isEmpty()) {
+      playerScore = addNewPlayer(player);
+      return playerScore;
+    } else {
+      playerScore = playerScoreList.stream().findFirst()
+          .map(ps -> ps.getPlayerName().equals(player.getName())
+              ? ps
+              : addNewPlayer(player)).orElse(playerScore);
+    }
+    playerScore.setGameTime(GAME_TIME);
+    return playerScore;
+  }
+
+  /**
+   * ゲームを始める前にプレイヤーの状態を設定する。
+   * 体力と空腹度を最大にして、装置はネザライト一式になる。
+   *
+   * @param player コマンドを実行したプレイヤー
+   */
+  private static void initPlayerStatus(Player player) {
+    player.setHealth(20);
+    player.setFoodLevel(20);
+    PlayerInventory inventory = player.getInventory();
+    inventory.setHelmet(new ItemStack(Material.NETHERITE_HELMET));
+    inventory.setChestplate(new ItemStack(Material.NETHERITE_CHESTPLATE));
+    inventory.setLeggings(new ItemStack(Material.NETHERITE_LEGGINGS));
+    inventory.setBoots(new ItemStack(Material.NETHERITE_BOOTS));
+    inventory.setItemInOffHand(new ItemStack(Material.SHIELD));
+    inventory.setItemInMainHand(new ItemStack(Material.NETHERITE_SWORD));
+  }
+
+  /**
+   * ゲームを実行します。規定の時間内に敵を倒すとスコアが加算されます。合計スコアを時間経過後に表示します。
+   *
+   * @param player コマンドを実行したプレイヤー
+   * @param nowPlayer プレイヤースコア情報
+   */
+  private void gamePlay(Player player, PlayerScore nowPlayer) {
+    Bukkit.getScheduler().runTaskTimer(main, Runnable -> {
+      if(nowPlayer.getGameTime() <=0){
+        Runnable.cancel();
+        player.sendTitle("ゲームが終了しました", nowPlayer.getPlayerName() + nowPlayer.getScore() + "点！", 0,60, 0);
+        nowPlayer.setScore(0);
+        List<Entity> nearbyEnemies = player.getNearbyEntities(50, 0, 50);
+        for(Entity enemy : nearbyEnemies){
+          switch (enemy.getType()) {
+            case ZOMBIE, SKELETON, SPIDER -> enemy.remove();
+          }
+        }
+        return;
+      }
+      player.getWorld().spawnEntity(getEnemySpawnLocation(player), getEnemy());
+      nowPlayer.setGameTime(nowPlayer.getGameTime()-5);
+    },0, 5*20);
+  }
+
+  /**
+   * 敵の出現エリアを取得します。
+   * 出現エリアはX軸、Z軸は自分の位置からプラス、ランダムで10−9の値が設定されます。
+   * Y軸はプレイヤーと同じ位置になります。
+   *
+   * @param player コマンドを実行したプレイヤー
+   * @return 敵の出現場所
+   */
+
+  private Location getEnemySpawnLocation(Player player) {
+    Location playerLocation = player.getLocation();
+    int randomX = new SplittableRandom().nextInt(20) - 10;
+    int randomZ = new SplittableRandom().nextInt(20) - 10;
+
+    double x = playerLocation.getX() + randomX;
+    double y = playerLocation.getY();
+    double z = playerLocation.getZ() + randomZ;
+    return new Location(player.getWorld(), x, y, z);
+  }
+
+  /**
+   * ランダムで敵を抽選して、その結果を取得します。
+   * @return 敵
+   */
+  private EntityType getEnemy() {
+    List<EntityType> enemyList = List.of(EntityType.ZOMBIE, EntityType.SKELETON, EntityType.SPIDER);
+    int random = new SplittableRandom().nextInt(enemyList.size());
+    return enemyList.get(random);
+  }
+
+}
